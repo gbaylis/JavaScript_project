54ab6a814baf79ebbc53f68e5ef72efe9c030f10
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-10 21:22:51 -0800

    Update index.js

--- brick breaker game/index.js
+++ brick breaker game/index.js
@@ -9,3 +9,14 @@ import Game from "/src/game";
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let game = new GAME(GAME_WIDTH, GAME_HEIGHT);
+
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+
+let lastTime = 0;
+function gameLoop(timestamp) {
+
