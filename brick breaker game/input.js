a8850ad280b94c12335dcd4b28786cae89fb34ec
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-15 11:00:21 -0800

    input.js

--- brick breaker game/input.js
+++ brick breaker game/input.js
@@ -1,1 +1,41 @@
 export default class InputHandler {
+  constructor(paddle, game) {
+    document.addWeventListener("keydown", event => {
+      switch (even.keyCode) {
+          
+          //case 37, refers to pressing the left key on the keyboard
+        case 37:
+        paddle.moveLeft();
+         break;
+          
+     // 39 refers to pressing the right arrow key on the keyboard     
+        case 39:
+          paddle.moveRight();
+          break;
+          
+        case 27:
+          game.togglePause();
+          break;
+          
+        case 32:
+          game.start();
+          break;
+      }
+    });
+    
+    document.addEventListener("keyup", event => {
+      switch (event.keyCode) {
+        case 37:
+         if (paddle.speed < 0) paddle.stop();
+          break;
+          
+        case 39:
+          if (paddle.speed > 0) paddle.stop();
+          break;
+      }
+    });
+  }
+}
+    
+    
+          
