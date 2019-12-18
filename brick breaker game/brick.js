4388016d59011aada94fc8afab07189de6251383
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-17 21:11:52 -0800

    Update brick.js

--- brick breaker game/brick.js
+++ brick breaker game/brick.js
@@ -5,3 +5,7 @@ date: Dec 17th 2019
 */
 
 import { detectCollision } from "./collisionDetection";
+
+export default class Brick {
+  constructor(game, position) {
+    this.image = document.getElementById("img_brick");
