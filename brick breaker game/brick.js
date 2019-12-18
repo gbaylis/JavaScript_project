be198c8d92fef8fd2e81d67a8ac2261229a96b45
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-17 21:14:55 -0800

    Update brick.js

--- brick breaker game/brick.js
+++ brick breaker game/brick.js
@@ -11,3 +11,16 @@ export default class Brick {
     this.image = document.getElementById("img_brick");
     
     this.game = game;
+    
+    this.position = position;
+    this.width = 80;
+    this.height = 24;
+    
+    this.markedForDeletion = false;
+  }
+  
+  update() {
+    if (detectCollision(this.game.ball, this)) {
+      this.game.ball.speed.y = -this.game.ball.speed.y;
+  
+  
