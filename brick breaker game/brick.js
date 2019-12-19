644b87f6a0f8bc619745f765c077045754256f57
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-18 22:19:09 -0800

    brick.js

--- brick breaker game/brick.js
+++ brick breaker game/brick.js
@@ -6,25 +6,21 @@ date: Dec 17th 2019
 
 import { detectCollision } from "./collisionDetection";
 
-export default class Brick {
-  constructor(game, position) {
-    this.image = document.getElementById("img_brick");
+export default class Ball {
+  constructor(game) {
+    this.image = document.getElementById("img_ball");
+    
+    this.gameWidth = gameWidth;
+    this.gameHeight = game.gameHeight;
     
     this.game = game;
-    
-    this.position = position;
-    this.width = 80;
-    this.height = 24;
-    
-    this.markedForDeletion = false;
+    this.size = 16;
+    this.reset();
   }
   
-  update() {
-    if (detectCollision(this.game.ball, this)) {
-      this.game.ball.speed.y = -this.game.ball.speed.y;
-      
-      this.markedForDeletion = true;
-    }
+  reset() {
+    this.position = { x: 10, y: 400 };
+    this.speed = { x: 4, y: -2 };
   }
   
   draw(ctx) {
@@ -32,11 +28,34 @@ export default class Brick {
       this.image,
       this.position.x,
       this.position.y,
-      this.width,
-      this.height
+      this.size,
+      this.size
       );
   }
+  
+  update(deltaTime) {
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+    
+    // wall on the left and right side of the screen
+    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+    
+    // wall or ceiling of the top of the screen
+    if (this.position.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
+    
+    // bottom of the screen
+    if (this.position.y + this.size > this.gameHeight) {
+      this.game.lives--;
+      this.reset();
+    }
+    
+    if (detectCollision(this, this.game.paddle)) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
+    }
+  }
 }
-  
-  
-  
