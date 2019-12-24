703632e41f3d02de8c5f3c0714ff9aa6cd85888e
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-23 21:36:09 -0800

    collisionDetection.js

--- brick breaker game/collisionDetection.js
+++ brick breaker game/collisionDetection.js
@@ -1,20 +1,25 @@
-export function detectCollision(ball, gameObject) { 
+/*
+Created by gbaylis
+Dec 23rd 2019
+*/
+
+export function detectCollision(ball, gameObject) {
   let bottomOfBall = ball.position.y + ball.size;
   let topOfBall = ball.position.y;
   
-  let topOfObject = gameObject.position.y;
+  let topofObject = gameObject.position.y;
   let leftSideOfObject = gameObject.position.x;
-  let rightSideOfObjet = gameObject.position.x + gameObject.width;
-  let bottomOfObject = gameObject.positio0n.y + gameObject.height;
+  let rightSideOfObject = gameObject.position.x + gameObject.width;
+  let bottomOfObject = gameObject.position.y + gameObject.height;
   
   if (
     bottomOfBall >= topOfObject &&
-    topOfBall <= bottomOfObject &&
+    topOfBall <= leftSideofObject &&
     ball.position.x >= leftSideOfObject &&
-    ball.position.x + ball.size <= rightSideObject
-    {
+    ball.position.x + ball.size <= rightSideOfObject
+    ) {
     return true;
-    } else {
+  } else {
     return false;
-    }
-    }
+  }
+}
