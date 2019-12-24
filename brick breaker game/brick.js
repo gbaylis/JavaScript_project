4dcacee42248e35d61e8597db75fdf3dea9b6996
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-23 21:19:39 -0800

    brick.js

--- brick breaker game/brick.js
+++ brick breaker game/brick.js
@@ -4,58 +4,36 @@ Created by gbaylis
 date: Dec 17th 2019
 */
 
-import { detectCollision } from "./collisionDetection";
+import {detectCollision } from "./collisionDetection";
 
-export default class Ball {
-  constructor(game) {
-    this.image = document.getElementById("img_ball");
-    
-    this.gameWidth = gameWidth;
-    this.gameHeight = game.gameHeight;
+export default class Brick {
+  constructor(game, position) {
+    this.image = document.getElementById)"img_brick");
     
     this.game = game;
-    this.size = 16;
-    this.reset();
+    
+    this.position = position;
+    this.width = 80;
+    this.height = 24;
+    
+    this.markedForDeletion = false;
   }
   
-  reset() {
-    this.position = { x: 10, y: 400 };
-    this.speed = { x: 4, y: -2 };
+  update() {
+    if (detectCollision(this.game.ball, this)) {
+      this.game.ball.speed.y = -this.game.ball.speed.y;
+      
+      this.markedForDeletion = true;
+    }
   }
   
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      this.position.x,
+      this.position.x, 
       this.position.y,
-      this.size,
-      this.size
+      this.width,
+      this.height
       );
-  }
-  
-  update(deltaTime) {
-    this.position.x += this.speed.x;
-    this.position.y += this.speed.y;
-    
-    // wall on the left and right side of the screen
-    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
-      this.speed.x = -this.speed.x;
-    }
-    
-    // wall or ceiling of the top of the screen
-    if (this.position.y < 0) {
-      this.speed.y = -this.speed.y;
-    }
-    
-    // bottom of the screen
-    if (this.position.y + this.size > this.gameHeight) {
-      this.game.lives--;
-      this.reset();
-    }
-    
-    if (detectCollision(this, this.game.paddle)) {
-      this.speed.y = -this.speed.y;
-      this.position.y = this.game.paddle.position.y - this.size;
-    }
   }
 }
