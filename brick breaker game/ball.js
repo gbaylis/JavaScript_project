8e239a42d5c336ae26c253d9c6de87834e6d96f0
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-25 21:15:43 -0800

    ball.js

--- brick breaker game/ball.js
+++ brick breaker game/ball.js
@@ -4,59 +4,27 @@ Dec 10, 2019
 JS - ball.js
 */
 
-import { detectCollision } from "./collisionDetection";
+import Game from "/src/game";
 
-export default class Ball {
-  constructor(game) {
-    this.image = document.getElementById("img_ball");
-    
-    this.gameWidth = game.gameWidth;
-    this.gameHeight = game.gameHeight;
-    
-    this.game = game;
-    this.size = 16;
-    this.reset();
-  }
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+
+let lastTime = 0;
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
   
-  reset() {
-    this.position = { x: 10, y: 400 };
-    this.speed = { x: 4, y: -2 };
-  }
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
   
-  draw(ctx) {
-    ctx.drawImage(
-      this.image,
-      this.position.x,
-      this.position.y,
-      this.size,
-      this.size
-      );
-  }
+  game.update(deltaTime);
+  game.draw(ctx);
   
-  update(deltaTime) {
-    this.position.x = += this.speed.x;
-    this.position.y += this.speed.y;
-    
-    // wall on the left and right side of the screen
-    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
-      this.speed.x = -this.speed.x;
-    }
-    
-    // wall on the top of the screen
-    'if (this.position.y < 0) {
-    this.speed.y = -this.speed.y;
-  }
-  
-  // bottom or floor of the game screen
-  if (this.position.y + this.size > this.gameHeight) {
-    this.game.lives--;
-    this.reset();
-  }
-  
-  if (detectCollision(this, this.game.paddle)) {
-    this.speed.y = -this.speed.y;
-    this.position.y = this.game.paddle.position.y - this.size;
-  }
- }
+  requestAnimationFrame(gameLoop);
 }
-      
+
+requestAnimationFrame(gameLoop);
