c4248688cf0041f289c3e5407c61432a6178a6bf
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-25 23:03:25 -0800

    game.js

--- brick breaker game/game.js
+++ brick breaker game/game.js
@@ -1,116 +1,86 @@
-import Paddle from "/src/paddle";
-import InputHandler from "/src/input";
-import Ball from "/src/ball";
-import Brick from "/src/brick";
+/*
+Created by gbaylis 
+Dec 24th 2019
+*/
 
+import Paddle from "/src/paddle";
+import inputHandler from "/src/input";
+import Ball from "/src/ball";
+import Brick from "/src/ball";
+
+// 2nd block of code
 import { buildLevel, level1, level2 } from "/src/levels";
 
+// 3rd block of code
 const GAMESTATE = {
-PAUSED: 0,
-RUNNING: 1,
-MENU: 2,
-GAMEOVER: 3,
-NEWLEVEL: 4
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3,
+  NEWLEVEL: 4
 };
 
+// 4th block of code
 export default class Game {
-constructor(gameWidth, gameHeight, bricksPerRow) {
-this.gameWidth = gameWidth;
-this.gameHeight = gameHeight;
-this.gamestate = GAMESTATE.MENU;
-this.ball = new BALL(this);
-this.paddle = new Paddle(this);
-this.gameObjects= [];
-this.bricks = [];
-this.lives = 3;
-
-this.levels = [level1, level2];
-this.currentLevel = 0;
-
-new InputHandler(this.paddle, this);
-}
-
-start() {
-if (
-  this.gamestate !== GAMESTATE.MENU &&
-  this.gamestate !== GAMESTATE.NEWLEVEL
-  )
-  return;  
+  constructor(gameWidth, gameHeight, bricksPerRow) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.gamestate = GAMESTATE.MENU;
+    this.ball = new Ball(this);
+    this.paddle = new Paddle(this);
+    this.gameObjects = [];
+    this.bricks = [];
+    this.lives = 3;
+    
+    // 5th block of code
+    this.levels = [level1, level2];
+    this.currentLevel = 0;
+    
+    // 6th block of code
+    new InputHandler(this.paddle, this);
+  }
   
-  this.bricks = buildLevel(this, this.levels[this.currentLevel]);
-  this.ball.reset();
-  this.gameObjects = [this.ball, this.paddle];
+  // 7th block of code
+  start() {
+    if (
+      this.gamestate !== GAMESTATE.MENU &&
+      this.gamestate !== GAMESTATE.NEWLEVEL
+      )
+      return;
+    
+    // 8th block of code
+    this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+    this.ball.reset();
+    this.gameObjects = [this.ball, this.paddle];
+    
+    // 9th block of code
+    this.gamestate = GAMESTATE.RUNNING;
+  }
   
-  this.gamestate = GAMESTATE.RUNNING;
-}
-  
+  // 10th block of code
   update(deltaTime) {
     if (this.lives === 0) this.gamestate = GAMESTATE.GAMEOVER;
-      
-      if (
-        this.gamestate === GAMESTATE.PAUSED ||
-        this.gamestate === GAMESTATE.MENU ||
-        this.gamestate ==- GAMESTATE.GAMEOVER
-        )
-        return;
-      
-      if (this.bricks.length === 0) {
-        this.currentLevel++;
-        this.gamestate = GAMESTATE.NEWLEVEL;
-        this.start();
-      }
-      
-      [...this.gameObjects, ...this.bricks].forEach(object =>
-      object.update(deltaTime)
-      );
-      
-      this.bricks = this.bricks.filter(brick => !brick.markedForDeletion);
+    
+    // 11th block of code
+    if (
+      this.gamestate === GAMESTATE.PAUSED ||
+      this.gamestate === GAMESTATE.MENU ||
+      this.gamestate === GAMESTATE.GAMEOVER
+      )
+      return;
+    
+    // 12th block of code
+    if (this.bricks.length === 0) {
+      this.currentLevel++;
+      this.gamestate = GAMESTATE.NEWLEVEL;
+      this.start();
     }
     
-    draw(ctx) {
-      [...this.gameObjects, ...this.bricks].forEach(object => object.draw(ctx));
-      
-if (this.gamestate === GAMESTATE.PAUSED) {
-  ctx.rect(0, 0, this.gameWidth, this.gameHeight);
-  ctx.fillStyle = "rgba(0,0,0,0.5)";
-  ctx.fill();
+    // 13th block of code
+    [...this.gameObjects, ...this.bricks].forEach(object =>
+    object.update(deltaTime)
+                                                  );
+    
+    // 14th block of code
+    this.bricks = this.bricks.filter(brick => !brick.markedForDeletion);
   
-  Text
-}
-      
-      if (this.gamestate === GAMESTATE.MENU) {
-        ctx.rect(0,0, this,gameWidth, this.gameHeight);
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctxx.fill();
-        
-  ctx.font = "30px Arial";
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-        ctx.fillText(
-          "Press SpaceBar to Start",
-        this.gameWidth / 2,
-          this.gameHeight / 2
-          );
-      }
-     
-      
-  If (this.gamestate === GAMESTATE.GAMEOVER) {
-    ctx.rect(0, 0, this.gameWidth, this.gameHeight);
-    ctx.fillStyle = "rgba(0,0,0,1)";
-    ctx.fill();
-    
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "white";
-ctx.textAlign = "center";
-    ctx.fillText("Game Over", this.gameWidth / 2, this.gameHeight / 2);
-  }
-    }
-    
-    togglePause() {
-      if (this.gamestate == GAMESTATE.PAUSED) {
-        this.gamestate = GAMESTATE.RUNNING;
-      } else {
-        this.gamestate = GAMESTATE.PAUSED;
-    }
-  }
-}
