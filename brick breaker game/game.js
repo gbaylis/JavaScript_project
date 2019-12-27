061790c00c7a225508d39388e51b5616833d5a5a
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-27 06:20:02 -0800

    game.js

--- brick breaker game/game.js
+++ brick breaker game/game.js
@@ -88,3 +88,46 @@ draw(ctx) {
     ctx.font = "30px Arial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
+    ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+  }
+  
+  // 17th block of code
+  if (this.gamestate === GAMESTATE.MENU) {
+    ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fill();
+    
+    // 18th block of code
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText(
+      "Press Space bar to Start the game",
+      this.gameWidth / 2,
+      this.gameHeight / 2
+      );
+  }
+  
+  // 19th block of code
+  if (this.gamestate === GAMESTATE.GAMEOVER) {
+    ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fill();
+    
+    // 20th block of code
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Game Over", this.gameWidth / 2, this.gameHeight / 2);
+  }
+}
+
+// 21st block of code
+togglePause() {
+  if (this.gamestate == GAMESTATE.PAUSED) {
+    this.gamestate = GAMESTATE.RUNNING;
+  } else {
+    this.gamestate = GAMESTATE.PAUSED;
+  }
+}
+}
