bbc5eaa8a63d33bef8b9e1e8e68eb3e909ae8e28
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-27 07:19:42 -0800

    level.js

--- brick breaker game/levels.js
+++ brick breaker game/levels.js
@@ -1,31 +1,43 @@
+/*
+created by gbaylis
+Dec 27th 2019
+*/
+
 import Brick from "/src/brick";
 
 export function buildLevel(game, level) {
-let bricks = [];
-
-level.forEach((row, rowIndex) => {
-row.forEach((brick, brickIndex) => {
-if (brick === 1) {
-  let position = {
-  x: 80 * brickIndex,
-  y: 75 + 24 * rowIndex
-  };
-  bricks.push(new Brick(game, position));
-  }
-  });
+  let bricks = [];
+  
+  level.forEach((row, rowIndex) => {
+    row.forEach((brick, brickIndex) => {
+     if (brick === 1) {
+       let position = {
+         x: 80 * brickIndex,
+         y: 75 + 24 * rowIndex
+       };
+       bricks.push(new Brick(game, position));
+     }
+    });
   });
   
   return bricks;
-  }
-  
-  export const level1 = [
-  // you can change the sequence of 0 and 1, but only use either 0 or 1
-  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+}
+
+export const level = [
+  // a 1 represents a brick in the row, a 0 means an empty space
+  [1, 0, 1, 0, 1, 0, 1, 0, 0, 0]
+  ];
+
+export const level2 = [
+  // you can change the brick patter, by changing 0 to 1 or vice versa
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+  [1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
   
-  export const level2 = [
-  [0, 1, 1, 0, 0, 0, 0, 1, 1, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-  ];
+  
+  
+  
+         
+
