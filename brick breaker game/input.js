c98313ac36b3114ae46a9e84a0930fabe3c1ee72
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-27 07:01:39 -0800

    input.js

--- brick breaker game/input.js
+++ brick breaker game/input.js
@@ -1,41 +1,48 @@
+/*
+created by gbaylis
+Dec 27th 2019
+*/
+
 export default class InputHandler {
   constructor(paddle, game) {
-    document.addWeventListener("keydown", event => {
-      switch (even.keyCode) {
-          
-          //case 37, refers to pressing the left key on the keyboard
-        case 37:
-        paddle.moveLeft();
-         break;
-          
-     // 39 refers to pressing the right arrow key on the keyboard     
-        case 39:
-          paddle.moveRight();
-          break;
-          
-        case 27:
-          game.togglePause();
-          break;
-          
-        case 32:
-          game.start();
-          break;
+    document.addEventListener("keydown", event => {
+      constructor(paddle, game) {
+        document.addEventListener("keydown", event => {
+          switch (even.keyCode) {
+              
+              // pressing the left arrow key
+            case 37:
+              paddle.moveLeft();
+              break;
+              
+              // pressing the right arrow key
+            case 39:
+              paddle.moveRight();
+              break;
+              
+              // pressing the space bar to pause the game
+            case 27:
+              game.togglePause();
+              break;
+              
+            case 32:
+              game.start();
+              break;
+          }
+        });
+        
+        document.addEventListener("keyup", event => {
+          switch (event.keyCode) {
+            case 37:
+              if (paddle.speed < 0) paddle.stop();
+              break;
+              
+              // case 39, when pressing the right arrow key on the keyboard
+              if (paddle.speed > 0) paddle.stop();
+              break;
+          }
+        });
       }
-    });
-    
-    document.addEventListener("keyup", event => {
-      switch (event.keyCode) {
-        case 37:
-         if (paddle.speed < 0) paddle.stop();
-          break;
-          
-        case 39:
-          if (paddle.speed > 0) paddle.stop();
-          break;
-      }
-    });
-  }
-}
-    
-    
-          
+    }
+            case 39:
+      
