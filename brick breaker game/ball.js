a142a67517fef12e0b9b3471c4f60a31cc13a904
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-27 21:06:10 -0800

    ball.js

--- brick breaker game/ball.js
+++ brick breaker game/ball.js
@@ -26,7 +26,6 @@ export default class Ball {
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      this.position.x,
       this.position.y,
       this.size,
       this.size
@@ -37,18 +36,17 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
     
-    // wall on the left and right side of the screen
-    
-    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+    // the wall on the left and right side of the screen
+    if ( this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
     
-    // the ceiling of the screen
+    // the ceiling or top wall of the game screen
     if (this.position.y < 0) {
       this.speed.y = -this.speed.y;
     }
     
-    // the floor or bottom of the screen
+    // the bottome of floor of the game screen
     if (this.position.y + this.size > this.gameHeight) {
       this.game.lives--;
       this.reset();
