03726b52ff0b1877b7f1d1cd0ba8b4c8be70ad7f
gbaylis <56212283+gbaylis@users.noreply.github.com>
2019-12-28 17:28:57 -0800

    index.js

--- brick breaker game/index.js
+++ brick breaker game/index.js
@@ -13,23 +13,19 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-let game = new GAME(GAME_WIDTH, GAME_HEIGHT);
-
-let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT):
 
 let lastTime = 0;
 function gameLoop(timestamp) {
-let deltaTIme = timestamp - lastTime;
+  let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
   
-ctx.clearReact(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
+  ctx.clerRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  
   game.update(deltaTime);
   game.draw(ctx);
   
   requestAnimationFrame(gameLoop);
 }
+
 requestAnimationFrame(gameLoop);
-  
-
-
